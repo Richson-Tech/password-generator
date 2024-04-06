a1da1b5911afd0739a734bb30b393c02cedf9581
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-06 18:37:15 +0100

    more commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Index from "./Index";
+import PasswordGenerator from "./PasswordGenerator";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Index />
+        <PasswordGenerator />
       </header>
     </div>
   );
